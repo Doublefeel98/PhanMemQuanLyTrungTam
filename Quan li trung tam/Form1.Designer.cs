ac235fbb723a27c1d16d7e792df784c99d477122
doublefeel98 <thanglong2098@gmail.com>
2018-04-11 09:29:33 +0700

    Add project files.

---
+++ Quan li trung tam/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Quan_li_trung_tam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSearchSV = new System.Windows.Forms.TextBox();
+            this.btnTim = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tvTrungTam = new System.Windows.Forms.TreeView();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.lvData = new System.Windows.Forms.ListView();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.panel1.Controls.Add(this.btnTim);
+            this.panel1.Controls.Add(this.txtSearchSV);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(844, 76);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(363, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nhập Tên:";
+            // 
+            // txtSearchSV
+            // 
+            this.txtSearchSV.Location = new System.Drawing.Point(510, 21);
+            this.txtSearchSV.Name = "txtSearchSV";
+            this.txtSearchSV.Size = new System.Drawing.Size(192, 36);
+            this.txtSearchSV.TabIndex = 1;
+            // 
+            // btnTim
+            // 
+            this.btnTim.Location = new System.Drawing.Point(720, 21);
+            this.btnTim.Name = "btnTim";
+            this.btnTim.Size = new System.Drawing.Size(112, 36);
+            this.btnTim.TabIndex = 2;
+            this.btnTim.Text = "Tìm";
+            this.btnTim.UseVisualStyleBackColor = true;
+            this.btnTim.Click += new System.EventHandler(this.btnTim_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.groupBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 76);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(375, 475);
+            this.panel2.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tvTrungTam);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(375, 475);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Các Trung Tâm:";
+            // 
+            // tvTrungTam
+            // 
+            this.tvTrungTam.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tvTrungTam.Location = new System.Drawing.Point(3, 32);
+            this.tvTrungTam.Name = "tvTrungTam";
+            this.tvTrungTam.Size = new System.Drawing.Size(369, 440);
+            this.tvTrungTam.TabIndex = 0;
+            this.tvTrungTam.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.tvTrungTam_AfterSelect);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.lvData);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(375, 76);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(469, 475);
+            this.panel3.TabIndex = 2;
+            // 
+            // lvData
+            // 
+            this.lvData.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvData.FullRowSelect = true;
+            this.lvData.GridLines = true;
+            this.lvData.HideSelection = false;
+            this.lvData.Location = new System.Drawing.Point(0, 0);
+            this.lvData.Name = "lvData";
+            this.lvData.Size = new System.Drawing.Size(469, 475);
+            this.lvData.TabIndex = 0;
+            this.lvData.UseCompatibleStateImageBehavior = false;
+            this.lvData.View = System.Windows.Forms.View.Details;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(844, 551);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnTim;
+        private System.Windows.Forms.TextBox txtSearchSV;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TreeView tvTrungTam;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.ListView lvData;
+    }
+}
+
---
+++ Quan li trung tam/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Quan_li_trung_tam
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string strConn = "Server=THAITHANG-PC;database=CSDL_QLTrungTam;User id=sa;pwd=thaithang1";
+        SqlConnection connTT = null;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadTrungTamLenTreeView();
+        }
+        SqlCommand CreateCommand(string sql, SqlConnection conn)
+        {
+            SqlCommand command = new SqlCommand();
+            command.CommandType = CommandType.Text;
+            command.CommandText = sql;
+            command.Connection = conn;
+            return command;
+        }
+        private void LoadTrungTamLenTreeView()
+        {
+
+            OpenConnection(ref connTT);
+            SqlCommand commandTT = CreateCommand("select * from TRUNGTAM", connTT);
+
+            SqlDataReader readerTT = commandTT.ExecuteReader();
+            tvTrungTam.Nodes.Clear();
+            while (readerTT.Read())
+            {
+                string matt = readerTT.GetString(0);
+                string tentt = readerTT.GetString(1);
+                TreeNode nodeTT = new TreeNode(tentt);
+                nodeTT.Tag = matt;
+
+                tvTrungTam.Nodes.Add(nodeTT);
+
+                SqlConnection connLop = null;
+                OpenConnection(ref connLop);
+                SqlCommand commandLop = CreateCommand("select * from LOP where MaTT ='" + matt + "'", connLop);
+                SqlDataReader readerLop = commandLop.ExecuteReader();
+                while (readerLop.Read())
+                {
+                    string malop = readerLop.GetString(0);
+                    string tenlop = readerLop.GetString(1);
+
+                    TreeNode nodeLop = new TreeNode(tenlop);
+                    nodeLop.Tag = malop;
+                    nodeTT.Nodes.Add(nodeLop);
+
+                    SqlConnection connSV = null;
+                    OpenConnection(ref connSV);
+                    SqlCommand commandSV = CreateCommand("select * from SINHVIEN where MaLop ='" + malop + "'", connSV);
+                    SqlDataReader readerSV = commandSV.ExecuteReader();
+                    while (readerSV.Read())
+                    {
+                        string masv = readerSV.GetString(0);
+                        string tensv = readerSV.GetString(1);
+
+                        TreeNode nodeSV = new TreeNode(tensv);
+                        nodeSV.Tag = masv;
+                        nodeLop.Nodes.Add(nodeSV);
+                    }
+                    readerSV.Close();
+                    CloseConnection(ref connSV);
+                }
+                readerLop.Close();
+                CloseConnection(ref connLop);
+            }
+            readerTT.Close();
+            CloseConnection(ref connTT);
+        }
+
+        private void OpenConnection(ref SqlConnection conn)
+        {
+            if (conn == null)
+                conn = new SqlConnection(strConn);
+            if (conn.State == ConnectionState.Closed)
+                conn.Open();
+        }
+        private void CloseConnection(ref SqlConnection conn)
+        {
+            if (conn != null && conn.State == ConnectionState.Open)
+                conn.Close();
+        }
+
+        private void tvTrungTam_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node != null)
+            {
+                if (e.Node.Level == 0)//nhấn vào nút trung tâm
+                {
+                    string matt = e.Node.Tag.ToString();
+                    HienThiLopLenListView(matt);
+                }
+                else if( e.Node.Level == 1)
+                {
+                    string malop = e.Node.Tag.ToString();
+                    HienThiSinhVienLenListView(malop);
+                }
+            }
+        }
+
+        private void HienThiSinhVienLenListView(string malop)
+        {
+            lvData.Items.Clear();
+            lvData.Columns.Clear();
+            lvData.Columns.Add("Mã SV", 100);
+            lvData.Columns.Add("Tên SV", 250);
+            lvData.Columns.Add("SĐT", 150);
+
+            SqlConnection conn = null;
+            OpenConnection(ref conn);
+            SqlCommand command = new SqlCommand();
+            command.CommandType = CommandType.Text;
+            command.CommandText = "select * from SINHVIEN where MaLop ='" + malop + "'";
+            command.Connection = conn;
+
+            SqlDataReader readerSV = command.ExecuteReader();
+            while (readerSV.Read())
+            {
+                string masv = readerSV.GetString(0);
+                string tensv = readerSV.GetString(1);
+                string sdt = readerSV.GetString(2);
+
+                ListViewItem lvi = new ListViewItem(masv);
+                lvi.SubItems.Add(tensv);
+                lvi.SubItems.Add(sdt);
+
+                lvData.Items.Add(lvi);
+            }
+            readerSV.Close();
+        }
+
+        private void HienThiLopLenListView(string matt)
+        {
+            lvData.Items.Clear();
+            lvData.Columns.Clear();
+            lvData.Columns.Add("Mã Lớp", 100);
+            lvData.Columns.Add("Tên Lớp", 300);
+
+            SqlConnection conn = null;
+            OpenConnection(ref conn);
+            SqlCommand command = new SqlCommand();
+            command.CommandType = CommandType.Text;
+            command.CommandText = "select * from LOP where MaTT ='" + matt + "'";
+            command.Connection = conn;
+
+            SqlDataReader readerLop = command.ExecuteReader();
+            while (readerLop.Read())
+            {
+                string malop = readerLop.GetString(0);
+                string tenlop = readerLop.GetString(1);
+
+                ListViewItem lvi = new ListViewItem(malop);
+                lvi.SubItems.Add(tenlop);
+
+                lvData.Items.Add(lvi);
+            }
+            readerLop.Close();
+        }
+
+        private void btnTim_Click(object sender, EventArgs e)
+        {
+            lvData.Items.Clear();
+            lvData.Columns.Clear();
+            lvData.Columns.Add("Mã SV", 100);
+            lvData.Columns.Add("Tên SV", 250);
+            lvData.Columns.Add("SĐT", 150);
+
+            SqlConnection conn = null;
+            OpenConnection(ref conn);
+            SqlCommand command = new SqlCommand();
+            command.CommandType = CommandType.Text;
+            command.CommandText = "select * from SINHVIEN where TenSV like @ten";
+            command.Connection = conn;
+
+            command.Parameters.Add("@ten", SqlDbType.NVarChar).Value = "%"+txtSearchSV.Text+"%";
+
+            SqlDataReader readerSV = command.ExecuteReader();
+            while (readerSV.Read())
+            {
+                string masv = readerSV.GetString(0);
+                string tensv = readerSV.GetString(1);
+                string sdt = readerSV.GetString(2);
+
+                ListViewItem lvi = new ListViewItem(masv);
+                lvi.SubItems.Add(tensv);
+                lvi.SubItems.Add(sdt);
+
+                lvData.Items.Add(lvi);
+            }
+            readerSV.Close();
+        }
+    }
+}
